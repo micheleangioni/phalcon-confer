046b7c5a6d1535bdb41c003f5784431608e8b329
Michele Angioni <michele.angioni@gmail.com>
2016-05-09 14:16:04 +0200

    Updated test bootstrap file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,8 +8,8 @@ $loader = new \Phalcon\Loader();
  * We register here the used directories, including the tests one, otherwise the TestCase couldn't be found.
  */
 $loader->registerNamespaces([
-    'MicheleAngioni\PhalconAuth' => dirname(__DIR__) . '/src/PhalconAuth',
-    'MicheleAngioni\PhalconAuth\Tests' => dirname(__DIR__) . '/tests'
+    'MicheleAngioni\PhalconConfer' => dirname(__DIR__) . '/src/PhalconConfer',
+    'MicheleAngioni\PhalconConfer\Tests' => dirname(__DIR__) . '/tests'
 ]);
 
 $loader->register();
